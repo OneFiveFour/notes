908125e951b32bf3975f9118cfe2148943a34619
Florian Meyer <florian.meyer@onefivefour.net>
2026-07-31 22:34:15 +0200

    Always display item creation pills in bottom navigation

--- composeApp/src/commonMain/kotlin/net/onefivefour/echolist/ui/home/BottomNavigation.kt
+++ composeApp/src/commonMain/kotlin/net/onefivefour/echolist/ui/home/BottomNavigation.kt
@@ -1,14 +1,9 @@
 package net.onefivefour.echolist.ui.home
 
-import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
@@ -16,45 +11,11 @@ import androidx.compose.ui.Modifier
 internal fun BottomNavigation(
     createItemCallbacks: CreateItemCallbacks = CreateItemCallbacks()
 ) {
-
-    var isFabExpanded by remember { mutableStateOf(false) }
-
-    Crossfade(targetState = isFabExpanded) { showItemPills ->
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            when (showItemPills) {
-                true -> {
-                    CreateItemPills(
-                        createItemCallbacks = CreateItemCallbacks(
-                            onCreateNote = {
-                                createItemCallbacks.onCreateNote()
-                                isFabExpanded = false
-                            },
-                            onCreateTaskList = {
-                                createItemCallbacks.onCreateTaskList()
-                                isFabExpanded = false
-                            },
-                            onCreateFolder = {
-                                createItemCallbacks.onCreateFolder()
-                                isFabExpanded = false
-                            }
-                        ),
-                        onClosePills = {
-                            isFabExpanded = false
-                        }
-                    )
-                }
-                else -> {
-                    BottomButtons(
-                        onOpenPills = {
-                            isFabExpanded = true
-                        }
-                    )
-                }
-            }
-        }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CreateItemPills(createItemCallbacks = createItemCallbacks)
     }
 }
